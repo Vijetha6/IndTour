c2f00cafb565582999a2dc5d1d5f925c0401a142
vijetha6 <vijethanayak6@gmail.com>
2025-08-19 21:44:57 +0530

    Initial project commit

---
+++ places.js
@@ -0,0 +1,20 @@
+let modeBtn=document.querySelector("#mode");
+let body=document.querySelector("body");
+let curMode="light";
+modeBtn.addEventListener("click",()=>
+    {
+        if(curMode=="light")
+        {
+            curMode='dark';
+            body.classList.add("dark");
+            body.classList.remove("light");
+        }
+        else
+            {
+                curMode='light';
+                body.classList.add("light");
+                body.classList.remove("dark");
+            }
+            console.log(curMode);
+        
+    });
